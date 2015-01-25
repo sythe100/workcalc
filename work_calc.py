0ab0b8ed42e2dc45f67952984ddac10b47fa11ef
sythe100 <sythe200@yahoo.com>
2015-01-25 15:14:21 -0500

    moved dictionary and prime_date variables to config.py

--- work_calc.py
+++ work_calc.py
@@ -3,45 +3,8 @@
 
 from datetime import date, datetime
 from workalendar.usa import Tennessee
+from config import PRIME_DATE, DAY_DICT
 
-PRIME_DATE = date(2014, 12, 1)
-DAY_DICT = {
-    1: "First day of days",
-    2: "Second day of days",
-    3: "Third day of days",
-    4: "Fourth day of days",
-    5: "First day of long break",
-    6: "Second day of long break",
-    7: "Third day of long break",
-    8: "Fourth day of long break",
-    9: "Fifth day of long break",
-    10: "Sixth day of long break",
-    11: "First night of nights (thurs)",
-    12: "Second night of nights (fri)",
-    13: "Third night of nights (sat)",
-    14: "Fourth night of nights (sun)",
-    15: "First day off between nights and hell week",
-    16: "Second day off between nights and hell week",
-    17: "Third day off between nights and hell week",
-    18: "Last day off between nights and hell week",
-    19: "First day of hell week",
-    20: "Second day of hell week",
-    21: "Third day of hell week",
-    22: "First night of hell week (mon)",
-    23: "Second night of hell week (tues)",
-    24: "Third night of hell week (wed)",
-    25: "First day off between hell week and training",
-    26: "Second day off between hell week and training",
-    27: "Third day off between hell week and training",
-    28: "Last day off between hell week and training",
-    29: "First day of training",
-    30: "Second day of training",
-    31: "Third day of training",
-    32: "Fourth day of training",
-    33: "Fifth day of training",
-    34: "First day off between training and days",
-    35: "Second day off between training and days",
-}
 cal = Tennessee()
 
 
